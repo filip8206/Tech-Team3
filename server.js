e9a71dc7f4c92507e5c7003a1e1faa3650c30cd0
filip8206 <fhensels@gmail.com>
2024-03-07 09:55:38 +0100

    env bij gitignore en server.js update

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 9000
+
+// Middleware
+app.use(express.static('public'))
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
