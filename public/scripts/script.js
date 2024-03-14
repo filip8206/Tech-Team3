28cbbe1e4a00a0358ca3bab0c8187a2efbc8e46b
Binc <bincvanburen@icloud.com>
2024-03-14 11:21:06 +0100

    home en filters

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -44,6 +44,7 @@ const toggle_sorteer = document.querySelector("#toggle_sorteer");
 
 });
 
+var body = document.body;
 
 
 
@@ -54,10 +55,62 @@ $ = function(id) {
 var show = function(id) {
 	$(id).style.display ='block';
   console.log("click ja");
+  body.classList.add("noscroll");
 }
 var hide = function(id) {
 	$(id).style.display ='none';
+  body.classList.remove("noscroll");
 }
+
+// RANGE SLIDER
+
+function getVals(){
+  // Get slider values
+  let parent = this.parentNode;
+  let slides = parent.getElementsByTagName("input");
+    let slide1 = parseFloat( slides[0].value );
+    let slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if( slide1 > slide2 ){ let tmp = slide2; slide2 = slide1; slide1 = tmp; }
+  
+  let displayElement = document.getElementsByClassName("rangeValues")[0];
+  displayElement.textContent = slide1 + " - " + slide2 + " BPM";
+}
+
+
+
+window.onload = function(){
+  // Initialize Sliders
+  let sliderSections = document.getElementsByClassName("range-slider");
+      for( let x = 0; x < sliderSections.length; x++ ){
+        let sliders = sliderSections[x].getElementsByTagName("input");
+        for( let y = 0; y < sliders.length; y++ ){
+          if( sliders[y].type ==="range" ){
+            sliders[y].oninput = getVals;
+            // Manually trigger event first time to display values
+            sliders[y].oninput();
+          }
+        }
+      }
+}
+
+
+// CHECKED FILTERS
+
+
+
+function updateCheckboxCount() {
+  var checkboxes = document.querySelectorAll('input[name="genre"]');
+  var checkedCount = 0;
+  checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+          checkedCount++;
+      }
+  });
+  document.getElementById('checkboxCount').textContent = checkedCount + ' geselecteerd';
+}
+
+
 
  
 
