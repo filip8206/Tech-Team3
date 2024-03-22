d22826fec0e18f4ba8998671999799c8c8c12d9d
filip8206 <fhensels@gmail.com>
2024-03-22 10:31:05 +0100

    profiel aanmaken

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-
+const session = require('express-session')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=techteam3`;
 
@@ -20,6 +20,17 @@ const client = new MongoClient(uri, {
 const bcrypt = require('bcrypt')
 const xss = require('xss')
 const saltRounds = 10
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/users/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.session.userID + path.extname(file.originalname));
+  }
+});
 
 // Middleware
 app.use(express.static('public'))
@@ -28,6 +39,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
+
+app.use(session({
+  secret: 'random string',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+const upload = multer({ storage: storage })
 
 console.log('Server gestart');
 
@@ -38,8 +57,10 @@ let incorrect
 
 app.get('/', async (req, res) => {
   res.render('index')
+  console.log(req.session.userID + ' is ingelogd')
 })
 
+/////////// Inloggen
 app.get('/inloggen', async (req, res) => {
   res.render('inloggen', { incorrect })
 })
@@ -50,17 +71,27 @@ app.post('/login', async (req,res) => {
   const coll = db.collection("users")
   const {email, password} = req.body
 
+  
+
   const user = await coll.findOne({ email: email })
+  if (user != null) {
   bcrypt.compare(password, user.password).then(function(result) {
     if(result === true){
+      console.log(user._id)
+      req.session.userID = user._id
+      req.session.email = user.email
       res.redirect('/')
     } else {
       incorrect = "Uw gebruikersnaam of wachtwoord is incorrect."
       res.redirect('/inloggen')
-    }
-  })
+    }})
+  } else {
+    incorrect = "Uw gebruikersnaam of wachtwoord is incorrect."
+    res.redirect('/inloggen')
+  }
 })
 
+////////// Registreren
 app.get('/registreer', async (req, res) => {
   res.render('registreer')
 })
@@ -97,18 +128,47 @@ app.post('/registreer', async (req, res) => {
         res.render('registreer', { foutmelding })
       } else {
 
-        console.log(req.body + 'geregistreerd')
-
         coll.insertOne({
           email: formEmail,
           password: hashedPassword,
           name: formName
         })
 
-        res.redirect('/klaar')
+        console.log(req.body + 'geregistreerd')
+        res.redirect('/profiel-aanmaken')
       }
     }
   }
+})
+
+////////// Profiel aanmaken
+app.get('/profiel-aanmaken', async (req, res) => {
+  const email = req.session.email
+  console.log ('user ' + email)
+  if (email != null) {
+  res.render('profiel-aanmaken', {email})
+  } else {
+    res.redirect('/inloggen')
+  }
+})
+
+app.post('/profiel-aanmaken', upload.single('profilephoto') , async (req,res) => {
+  const db = client.db('muve')
+  const coll = db.collection('users')
+
+  const formDescription = xss(req.body.description)
+  console.log(req.file)
+  console.log(req.body.genres)
+  console.log(formDescription)
+  const user = await coll.findOne({ email: req.session.email })
+  console.log(user)
+  coll.updateOne({ email: req.session.email},{ $set: {
+    profilePicturePath: req.file.path,
+    description: formDescription,
+    genres: req.body.genres
+}})
+  console.log('profiel bijgewerkt')
+  res.redirect('/')
 })
 
 app.get('/klaar', async (req, res) => {
