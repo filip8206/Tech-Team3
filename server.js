13aa77257c815d1c481561183ddea69f372b268a
lorenzohorden <122381790+lorenzohorden@users.noreply.github.com>
2024-03-22 11:44:54 +0100

    nieuwe genres toegevoegd aan filter

--- server.js
+++ server.js
@@ -59,13 +59,12 @@ app.get('/', async (req,res) => {
 app.post('/', async (req,res) => {
   let genre, key = []
   genre = req.body.genre
-  if(genre === undefined){genre=["pop", "dutch"]}
+  if(genre === undefined){genre=["pop", "dutch", "rap", "rock"]}
   key = req.body.key
   if(key === undefined){key=["a", "b", "c", "d", "e", "f", "g"]}
   const {sorteren, bpmMin, bpmMax} = req.body
   const bpm = [bpmMin, bpmMax]
   const url = `/?key=${key}&genre=${genre}&sorteren=${sorteren}&bpm=${bpm}`
-  console.log(key, genre, sorteren, bpm)
   res.redirect(url)
 })
 
